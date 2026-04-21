f86e2a94bedf1c4550d80c74f0041e36259eaa98
xtrem3440-cool <xtrem3440@gmail.com>
2026-04-21 13:11:27 +0530

    Add files via upload

--- src/hacks/Global/GameBrightness.cpp
+++ src/hacks/Global/GameBrightness.cpp
@@ -1,6 +1,6 @@
 #include <modules/config/config.hpp>
 #include <modules/gui/gui.hpp>
-<parameter name="components/float-toggle.hpp">
+#include <modules/components/float-toggle.hpp>
 #include <modules/hack/hack.hpp>
 
 namespace eclipse::hacks::Global {
